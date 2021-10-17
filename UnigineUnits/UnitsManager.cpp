def8412782b717e5ab81f8a743afaec6c8b9e1b5
Константин <k-o-t-111@mail.ru>
2021-10-18 02:12:13 +0700

    Откат от указателей в многопотоке, переход к копированию...

--- UnigineUnits/UnitsManager.cpp
+++ UnigineUnits/UnitsManager.cpp
@@ -6,7 +6,7 @@ namespace
 {
 //-------------------------------------------------------
 // Поиск юнитов в секторе обзора
-void FindUnitsInViewSector(std::vector<DefaultUnit*>& tempUnit, const std::vector<DefaultUnit>& originalUnits)
+void FindUnitsInViewSector(std::vector<DefaultUnit>& tempUnit, const std::vector<DefaultUnit>& originalUnits)
 {
 	// Из-за потенциального изменения позиций юнитов не стал упорядочивать данные,
 	// поэтому каждый раз перебираю весь массив юнитов, ускорение достигается потоками
@@ -15,12 +15,12 @@ void FindUnitsInViewSector(std::vector<DefaultUnit*>& tempUnit, const std::vecto
 		for (const auto& originalUnit : originalUnits)
 		{
 			// Игнорируем самого себя
-			if (&*unit == &originalUnit)
+			if (&unit == &originalUnit)
 				continue;
 
 			// Проверяем нахождение в видимости
-			if (unit->IsInViewSector(originalUnit))
-				unit->AddVisibleUnit();
+			if (unit.IsInViewSector(originalUnit))
+				unit.AddVisibleUnit();
 
 		}
 	}
@@ -36,7 +36,7 @@ const bool UnitsManager::ManageUnits(const std::string& filename)
 		return false;
 
 	
-	std::vector<std::vector<DefaultUnit*>> tempVectors;
+	std::vector<std::vector<DefaultUnit>> tempVectors;
 	std::vector<std::thread> threads;
 	int threadsCount = 0;
 	std::cout << "Input theards count: ";
@@ -47,20 +47,28 @@ const bool UnitsManager::ManageUnits(const std::string& filename)
 		std::cout << "More threads than units! Threads count will be 1\n";
 	}
 	// Из-за нехватки знаний для решения проблем доступа в многопоточности
-	// использую разделение массива юнитов на число потоков 
+	// использую разделение массива юнитов на число потоков
+	int perThread = m_units.size() / threadsCount;
 	for (int thread = 0; thread < threadsCount; ++thread)
 	{
-		std::vector<DefaultUnit*> tempVector;
-		int maxSize = (m_units.size() / threadsCount) * (thread + 1);
-		for (int i = (m_units.size() / threadsCount) * thread ; i < maxSize; i++)
-			tempVector.push_back(&m_units[i]);
-		tempVectors.emplace_back(tempVector);
+		std::vector<DefaultUnit> tempVector;
+		tempVector.clear();
+		for (int i = perThread  * thread ; i < perThread * (thread + 1); i++)
+			tempVector.push_back(m_units[i]);
+		tempVectors.push_back(tempVector);
 		threads.push_back(std::thread(FindUnitsInViewSector, ref(tempVectors[thread]), ref(m_units)));
 	}
 
 	for (auto& thr : threads) 
 	{
 		thr.join();
+	}
+
+	m_units.clear();
+	for (const auto& vector : tempVectors)
+	{
+		for (const auto& unit : vector)
+			m_units.emplace_back(unit);
 	}
 
 	// Сохранение полученной информации о юнитах
